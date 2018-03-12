0e7d18e2fa6b1c6722d79e412455e97a4e829f10
Luke Chambers <lukenatchambers@gmail.com>
2018-03-12 17:45:26 -0600

    Start building out items endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312234907) do
+ActiveRecord::Schema.define(version: 20180312233609) do
 
-  create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "merchants", force: :cascade do |t|
-    t.string "name"
+  create_table "items", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.integer "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
