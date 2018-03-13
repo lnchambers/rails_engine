d2854ccabaaf4a64c979a37f480174603f3c9fb9
mgmilton <matt.milton@gmail.com>
2018-03-13 13:48:52 -0600

    updates merchant spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -26,10 +26,41 @@ describe "Merchants API" do
     expect(response.body).to match(merchant.to_json)
   end
 
-  it "can locate a single merchant" do
-    id = create(:merchant).id
+  it "can locate a single merchant by name" do
+    create_list(:merchant, 4, name: "Elizabeth-Blackburn")
+    create_list(:merchant, 5)
 
+    get "/api/v1/merchants/find?name=Elizabeth-Blackburn"
 
+    merchant = JSON.parse(response.body)
 
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+    expect(response.body).to match(merchant.to_json)
+  end
+
+  it "can locate multiple merchants by name" do
+    create_list(:merchant, 4, name: "Elizabeth-Blackburn")
+
+    get "/api/v1/merchants/find_all?name=Elizabeth-Blackburn"
+
+    merchant = JSON.parse(response.body)
+    expect(merchant.count).to eq(4)
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+    expect(response.body).to match(merchant.to_json)
+  end
+
+  it "can locate a random merchant" do
+    create_list(:merchant, 10)
+
+    get "/api/v1/merchants/random"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+    expect(merchant.class).to eq(Hash)
+    expect(response.body).to match(merchant.to_json)
   end
 end
