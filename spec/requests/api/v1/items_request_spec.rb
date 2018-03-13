ce8fe4968839580016c3850b7dd79bbe40ccde21
Luke Chambers <lukenatchambers@gmail.com>
2018-03-12 18:24:21 -0600

    Show API for items is functional\

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -14,4 +14,18 @@ describe "Items Request" do
     expect(items[0]["description"]).to eq("Humphry Davenport The Second; Esquire, Duke of Portsholdshirelington")
     expect(items[0]["unit_price"]).to eq(4999)
   end
+
+  it "can show you a single item" do
+    create_list(:item, 10)
+
+    get "/api/v1/items/5"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["name"]).to eq("Opakawagalaga Eupanifahorious")
+    expect(item["description"]).to eq("Humphry Davenport The Second; Esquire, Duke of Portsholdshirelington")
+    expect(item["unit_price"]).to eq(4999)
+    expect(item[1]).to eq(nil)
+  end
 end
