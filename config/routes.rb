175762f90052823a86e7b91dba5da5993c9bf8cd
Luke Chambers <lukenatchambers@gmail.com>
2018-03-12 18:53:09 -0600

    Add routes back

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :transactions, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      get "items/find", :to => "items#show"
     end
   end
 end
