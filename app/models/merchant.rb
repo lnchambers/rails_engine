f5b0292ae22d768fe8d4b8a6b2f8a461a8c85fcb
Luke Chambers <lukenatchambers@gmail.com>
2018-03-14 16:28:11 -0600

    Build out some dream driven development

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,8 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 
   def self.most_revenue(limit = 5)
     select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue")
