7eacdbad127b14f8f3f31c4724e76ab524d3cb1b
Luke Chambers <lukenatchambers@gmail.com>
2018-03-14 09:58:40 -0600

    Customer relationships passing

--- app/controllers/api/v1/customers/invoices_controller.rb
+++ app/controllers/api/v1/customers/invoices_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Customers::TransactionsController < ApplicationController
+class Api::V1::Customers::InvoicesController < ApplicationController
 
   def index
     customer = Customer.find(params[:customer_id])
