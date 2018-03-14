dcc7d605b1671fadd174eb3492c6269048a6f26a
Luke Chambers <lukenatchambers@gmail.com>
2018-03-13 21:42:38 -0600

    Build out test for customer/:id/transaction

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
-      resources :customers, only: [:index, :show]
+      resources :customers, only: [:index, :show] do
+        resources :transactions, only: [:index]
+      end
       resources :transactions, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
---
+++ spec/requests/api/v1/customers/transaction_request_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Customer Transaction Request" do
+  it "can return all transactions under a specific customer" do
+    invoice = create(:invoice)
+    transaction = create(:transaction, invoice: invoice)
+    create_list(:transaction, 4, invoice: invoice)
+
+
+    get "/api/v1/customers/#{transaction.invoice.customer_id}/transaction"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.count).to eq(5)
+    expect(transactions[0]["credit_card_number"]).to eq(transaction.credit_card_number)
+    expect(transactions[0]["result"]).to eq(transaction.result)
+  end
+end
