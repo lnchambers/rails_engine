715fea267cb1b072b756f39e5fe84c5b01a243e4
Luke Chambers <lukenatchambers@gmail.com>
2018-03-14 10:21:21 -0600

    Transaction relationship endpoint complete

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::Customers::TransactionsController < ApplicationController
 
   def index
-    customer = Customer.find(params[:customer_id])
-    render json: customer.transactions
+    render json: Customer.find(params[:customer_id]).transactions
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
         get "transactions", :to => "customers/transactions#index"
         get "invoices", :to => "customers/invoices#index"
       end
-      resources :transactions, only: [:index, :show]
+      resources :transactions, only: [:index, :show] do
+        get "invoice", :to => "transactions/invoices#show"
+      end
       resources :items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
