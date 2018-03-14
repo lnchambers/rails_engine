f9b7f1b4a4b1abf409effbdc3c02f491859831d0
mgmilton <matt.milton@gmail.com>
2018-03-14 12:02:25 -0600

    adds serializers

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,20 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
   has_many :invoice_items
-  has_many :items, through: :invoice_items
   has_many :transactions
+  has_many :items, through: :invoice_items
+<<<<<<< HEAD
+  has_many :transactions
+=======
+
+  def self.most_expensive(limit = 5)
+    select("invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+      .joins(:invoice_items, :transactions)
+      .merge(Transaction.unscoped.successful)
+      .order("revenue DESC").limit(limit)
+      .group(:id)
+  end
+
+
+>>>>>>> adds serializers
 end
