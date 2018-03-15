255bfd8d5e7cdb901b3af7d8eb7fd72e2c0a9532
mgmilton <matt.milton@gmail.com>
2018-03-15 09:49:26 -0600

    adds top revenue for all merchants and most items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,18 +8,18 @@ class Merchant < ApplicationRecord
 
   def self.most_revenue(limit = 5)
     select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue")
-      .joins(:invoice_items, :transactions)
+      .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.unscoped.successful)
       .order("revenue DESC").limit(limit)
       .group(:id)
   end
 
   def self.most_items(limit = 5)
-    select("merchants.*, sum(invoice_items.item_id * invoice_items.quantity) AS solditems")
-      .joins(:invoice_items, :transactions)
+    select("merchants.*, sum(invoice_items.quantity) AS solditems")
+      .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.unscoped.successful)
-      .order("solditems DESC").limit(limit)
       .group(:id)
+      .order("solditems DESC").limit(limit)
   end
 
   def self.revenue_by_date(date)
@@ -44,10 +44,10 @@ class Merchant < ApplicationRecord
     start_day = DateTime.parse(date).beginning_of_day
     end_day  = DateTime.parse(date).end_of_day
     invoice_items
-    .joins(invoice: :transactions)
-    .merge(Transaction.unscoped.successful)
-    .where(invoices: {updated_at: start_day..end_day})
-    .sum("unit_price * quantity")
+      .joins(invoice: :transactions)
+      .merge(Transaction.unscoped.successful)
+      .where(invoices: {updated_at: start_day..end_day})
+      .sum("unit_price * quantity")
   end
 
 end
