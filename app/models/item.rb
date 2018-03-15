48f59da26c49ed972b0c8f48f885a80984eda189
mgmilton <matt.milton@gmail.com>
2018-03-14 22:04:03 -0600

    adds most_items method for all items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_items(limit = 5)
+    select("items.*, count(invoice_items.item_id) as solditems")
+    .joins(:invoice_items)
+    .order("solditems DESC").limit(limit)
+    .group(:id)
+  end
 end
