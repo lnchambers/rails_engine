017c2d1e8fbf5a442b3f7156e6eadb0dffe903a2
mgmilton <matt.milton@gmail.com>
2018-03-15 11:35:55 -0600

    adds transaction relationship

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,6 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
   has_many :invoices
+  has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
-
 end
