e5c27eca2b2ccdaf6c4c274bcaca81c889ae7332
mgmilton <matt.milton@gmail.com>
2018-03-15 14:39:27 -0600

    adds credit card to string method in serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,7 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :credit_card_number, :result
+  attributes :id, :credit_card_number, :invoice_id, :result
+
+  def credit_card_number
+    object.credit_card_number.to_s
+  end
 end
